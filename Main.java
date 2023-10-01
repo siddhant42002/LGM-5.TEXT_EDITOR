41428c45ba3ec20562aac1d7ec8dc6fc2f6f04cc
wakchaure Siddhant Sanjay <89894389+siddhant42002@users.noreply.github.com>
2023-10-01 10:46:07 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class Main {
+    public static void main(String[] args) {
+
+        TextEditor textEditor = new TextEditor();
+        textEditor.setTitle("Text Editor");
+        textEditor.setSize(800, 600);
+        textEditor.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        textEditor.setVisible(true);
+    }
+}
+
+
